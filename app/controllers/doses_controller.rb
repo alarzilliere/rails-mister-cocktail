3a35ab2719541884741c550d30c83782eb79046b
anthony larzilliere <larzilliere.anthony@gmail.com>
2017-05-11 21:34:45 +0200

    finalisation des views

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,26 @@
 class DosesController < ApplicationController
   def new
+
+      @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render "cocktail/show"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
